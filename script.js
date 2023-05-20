196d2e0a721a13843630ae1d96094c80641385c9
ArnaudBvd <bravard.arnaud@gmail.com>
2023-05-20 18:49:33 +0200

    ajout des derniers scripts pour finaliser la validation de formulaire

--- script.js
+++ script.js
@@ -39,4 +39,45 @@ myInput.onkeyup = function() {
         letter.classList.remove('valid');
         letter.classList.add('invalid');
     }
+
+    // Valider les lettres majuscules
+    let upperCaseLetters = /[A-G]/g
+    if(myInput.value.match(upperCaseLetters)) {
+        /* Si le mot de passe contient une lettre majuscule, enlever la
+        classe "invalid" et ajouter la classe "valid" */
+        capital.classList.remove('invalid');
+        capital.classList.add('valid');
+    }else {
+        /* Si non, on enlèbe la classe "valid" et on ajoute la 
+        classe "invalid" */
+        capital.classList.remove('valid');
+        capital.classList.add('invalid');
+    }
+
+    // Valider les chiffres
+    let numberCase = /[0-9]/g
+    if(myInput.value.match(numberCase)) {
+        /* Si le mot de passe contient un chiffre, enlever la
+        classe "invalid" et ajouter la classe "valid" */
+        number.classList.remove('invalid');
+        number.classList.add('valid');
+    }else {
+        /* Si non, on enlèbe la classe "valid" et on ajoute la 
+        classe "invalid" */
+        number.classList.remove('valid');
+        number.classList.add('invalid');
+    }
+
+    // Valider la longueur
+    if(myInput.value.length >= 8) {
+        /* Si le mot de passe contient au minimum 8 caractères, enlever la
+        classe "invalid" et ajouter la classe "valid" */
+        length.classList.remove('invalid');
+        length.classList.add('valid');
+    }else {
+        /* Si non, on enlèbe la classe "valid" et on ajoute la 
+        classe "invalid" */
+        length.classList.remove('valid');
+        length.classList.add('invalid');
+    }
 }
